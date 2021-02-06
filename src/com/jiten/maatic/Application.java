fc559be321aeb331f794a79c28051595742ca884
jitendra <netij.poudel@gmail.com>
2021-02-06 15:07:13 -0600

    adding project

---
+++ src/com/jiten/maatic/Application.java
@@ -0,0 +1,18 @@
+package com.jiten.maatic;
+
+public class Application {
+
+	public static void main(String[] args) {
+		
+		/*JavaFirstDay jfd=new JavaFirstDay();
+		//jfd.add(12,13);
+		//System.out.println(sumValue);
+		
+		//String a=jfd.print();
+		System.out.println(jfd.print());
+		*/
+		
+		
+	}
+
+}
---
+++ src/com/jiten/maatic/JavaFirstDay.java
@@ -0,0 +1,13 @@
+package com.jiten.maatic;
+
+public class JavaFirstDay {
+
+	//int a = 20;
+	//int b = 30;
+
+	
+	public String print() {
+
+		return "Jiten";
+	}
+}
